b5343ce57b90790723fbf200518e7b19b98c6cc6
pratikpatil <pratikpatil9514@gmail.com>
2024-12-27 15:55:41 +0530

    This is my second commit

---
+++ First.java
@@ -0,0 +1,13 @@
+class One{
+
+public static void main(String[] args){
+	System.out.println("Welcome to Java");
+	}
+
+}
+
+class Two{
+	public static void main(String[] args){
+	System.out.println("Welcome to Java Training");
+	}
+}
